769f1fb7a1c9a0b81e70d3d4ff52f702b1789b51
Need4Swede <theneed4swede@gmail.com>
2022-06-21 13:37:45 -0700

    Create plusminus.py

---
+++ HackerRank/plusminus.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    # Number of elements
+    arr_length = len(arr)
+    
+    # Value Sums
+    positives, negatives, zeroes = 0, 0, 0
+    
+    # Iterate through array
+    for number in range(arr_length):
+        if arr[number] > 0:
+            positives += 1
+        elif arr[number] == 0:
+            zeroes += 1
+        else:
+            negatives += 1
+    
+    # Get averages
+    positives = positives / arr_length
+    negatives = negatives / arr_length
+    zeroes = zeroes / arr_length
+    
+    # Print to console
+    print(f"{positives:.6f}")
+    print(f"{negatives:.6f}")
+    print(f"{zeroes:.6f}")
+        
+        
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
