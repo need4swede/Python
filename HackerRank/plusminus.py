a1f29b6b604925ab7ac2ee8ebc615459fc6b3bda
Need4Swede <theneed4swede@gmail.com>
2022-06-21 13:53:28 -0700

    Update plusminus.py

--- HackerRank/plusminus.py
+++ HackerRank/plusminus.py
@@ -1,3 +1,7 @@
+# Given an array of integers, calculate the ratios of its elements 
+# that are positive, negative, and zero. Print the decimal value of 
+# each fraction on a new line with 6 places after the decimal.
+
 #!/bin/python3
 
 import math
