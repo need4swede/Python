ed80fd83f24fe0c23496cbece5f7992e704f88ce
Need4Swede <theneed4swede@gmail.com>
2022-06-22 12:16:06 -0700

    Create lonely_integer.py

---
+++ HackerRank/lonely_integer.py
@@ -0,0 +1,34 @@
+# Given an array of integers, where all elements but one occur twice, find the unique element.
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    num = [i for i in a if a.count(i) < 2]
+    return str(num[0])
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
