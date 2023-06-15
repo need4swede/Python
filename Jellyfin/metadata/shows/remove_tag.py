46cf023799582985d0603d6c0408636b51e9ec19
Need4Swede <theneed4swede@gmail.com>
2023-06-15 16:18:59 -0700

    Update remove_tag.py

--- Jellyfin/metadata/shows/remove_tag.py
+++ Jellyfin/metadata/shows/remove_tag.py
@@ -1,9 +1,14 @@
 import os
 
 class Jellyfin:
+
+    # Searches show directory for 'tvshow.nfo' files
     def process_directory(self, directory, tag):
+
+        # Include folders you do not want to include in your search
         skip_list = {os.path.join(directory, "Game of Thrones (2011)")}
 
+        # Begin search
         for root, dirs, files in os.walk(directory):
             if root in skip_list:
                 print(f"Skipped: {root}")
@@ -15,15 +20,20 @@ class Jellyfin:
                     self.process_nfo_file(file_path, tag)
                     print(f"Processed: {root}")
 
+    # Removes tag found 'tvshow.nfo' file
     def process_nfo_file(self, file_path, tag):
-        lines = []
+
+        tvshow_nfo = []
+
+        # Add every line except our selected tag
         with open(file_path, 'r', encoding='utf-8') as file:
             for line in file:
                 if f"<tag>{tag}</tag>".strip() not in line.strip():
-                    lines.append(line)
+                    tvshow_nfo.append(line)
 
+        # Overwrite nfo file
         with open(file_path, 'w', encoding='utf-8') as file:
-            file.writelines(lines)
+            file.writelines(tvshow_nfo)
 
 if __name__ == "__main__":
     show_directory = input("Enter the directory path for your Jellyfin's show library: ").strip()
