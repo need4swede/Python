ae073df607860e959760cfeb7c6aba557b1abb73
Need4Swede <57847545+need4swede@users.noreply.github.com>
2023-06-19 11:48:51 -0700

    Create find_nfo.py

---
+++ Jellyfin/metadata/all/find_nfo.py
@@ -0,0 +1,30 @@
+import os
+
+class Jellyfin():
+
+    # Searches show directory for '.nfo' files
+    def process_directory(self, directory):
+
+        # Begin search
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file.endswith('.nfo'):
+                    nfo_file = os.path.join(root, file)
+                    self.write_to_file(nfo_file, directory)
+        print('---\nDone')
+
+    # Writes .nfo file location to text
+    def write_to_file(self, text, media_dir):
+        nfo_text_file = os.path.join(media_dir, 'nfo_files.txt')
+        with open(nfo_text_file, 'a+', encoding='utf-8') as textFile:
+            textFile.write(text + '\n')
+
+if __name__ == "__main__":
+
+    # Root directory path of where your media is stored (the parent folder of your Movies/Shows directories)
+    media_directory = input("Enter the directory path for your Jellyfin's media library: ")
+    media_directory = r"{dir}".format(dir=media_directory)
+
+    # Begin instance
+    jf = Jellyfin()
+    jf.process_directory(media_directory)
